6ae8044fc74a564e8073d422a4766f8daa40cfe6
perry <User@Tipzee-Aspire.(none)>
2019-04-07 17:53:59 +0800

    Offloaded validation result to a function

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,8 +3,11 @@ const { validationResult } = require('express-validator/check');
 const boom = require('boom');
 
 const _this = {
+    _getValidationResult: (req) => {
+        return validationResult(req);
+    },
     validationHandler: (req, res, next) => {
-        const errors = validationResult(req);
+        const errors = _this._getValidationResult(req);
         
         if (!errors.isEmpty()) {
             next(boom.badRequest(false, errors.array()))
@@ -15,7 +18,7 @@ const _this = {
         UserService
             .saveUser(req.body.email, req.body.username, req.body.password)
             .then(result => res.status(200).send(result))
-            .catch(error => next(error));
+            .catch(error => next(error));        
     },
     index: (req, res) => {
         res.status(200).send('Welcome');
