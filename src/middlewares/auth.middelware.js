ec9efc3b24a00e8175e673702ae1c901da365d01
perry <User@Tipzee-Aspire.(none)>
2019-04-07 22:25:39 +0800

    Deleted

--- src/middlewares/auth.middelware.js
+++
@@ -1,9 +0,0 @@
-const { check } = require('express-validator/check');
-const controller = require('../controllers/users.controller');
-const boom = require('boom')
-
-exports.auth = [
-    check('username').exists(),
-    check('password').isLength({ min: 5 }).withMessage("Must be 5 characters long"),
-    controller.validationHandler(req, res, next)
-];
