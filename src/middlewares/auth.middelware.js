8d067053b0a247dbabfac2062b70aab33b804d1f
perry <User@Tipzee-Aspire.(none)>
2019-04-07 17:55:54 +0800

    rename

--- src/middlewares/auth.middelware.js
+++ src/middlewares/auth.middelware.js
@@ -1,9 +1,9 @@
 const { check } = require('express-validator/check');
-const handlers = require('../../routes/auth/handlers');
+const controller = require('../controllers/users.controller');
 const boom = require('boom')
 
 exports.auth = [
     check('username').exists(),
     check('password').isLength({ min: 5 }).withMessage("Must be 5 characters long"),
-    handlers.validationHandler(req, res, next)
+    controller.validationHandler(req, res, next)
 ];
